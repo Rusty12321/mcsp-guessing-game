e64b9eb150a42119ccacc1f8217ebefd4deb7e13
Rusty12321 <weston@hilgenfeld.org>
2023-03-01 16:33:06 -0600

    alerts and prompts more readable

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 let users = {};
+let guessCount;
+const playAgainMessage = `Would you like to play again? "yes" or "no"`;
 
 function mainGame() {
     let name = prompt("Please enter your name.");
+    let higherMessage = `Sorry ${name}, guess higher`;
+    let lowerMessage = `Sorry ${name}, guess lower`;
     let secretNumber = Math.floor(Math.random() * (100 - 1) + 1);
     let answer = Number.parseInt(prompt("Guess what the secret number is between 1 and 100."));
-    let guessCount = 1
     let guesses = [];
     let playAgain = "yes";
+    guessCount = 0;
 
-    while (playAgain === "yes") {
+    while (playAgain.toLocaleLowerCase() === "yes") {
+        guesses.push(answer);
+        guessCount++;
         if (!Number.isInteger(answer)) {
             answer = Number.parseInt(prompt(`Sorry ${name}, that is not a number. please guess a number between 1 and  100`));
             continue;
@@ -18,32 +24,56 @@ function mainGame() {
             continue;
         }
         if (answer === secretNumber) {
+            guesses.pop();
+            let pluralOrNotGuessCount;
+            let pluralOrNotBetter;
+            let pluralOrNotWorse;
+            let better = guessCount - users[name];
+            let worse = users[name] - guessCount;
+            if (guessCount > 1) {
+                pluralOrNotGuessCount = 'guesses';
+            } else {
+                pluralOrNotGuessCount = 'guess';
+            }
+            if (better > 1) {
+                pluralOrNotBetter = 'guesses';
+            } else {
+                pluralOrNotBetter = 'guess';
+            }
+            if (worse > 1) {
+                pluralOrNotWorse = 'guesses';
+            } else {
+                pluralOrNotWorse = 'guess';
+            }
             if (users[name] === undefined) {
-                alert(`That's correct ${name}! It took you ${guessCount} guesses. Your previous guesses were ${guesses}!`);
+                if (guessCount > 1) {
+                    alert(`That's correct ${name}! It took you ${guessCount} guesses. You previously guessed ${guesses}!`);
+                } else {
+                    alert(`That's correct ${name}! It took you ${guessCount} guess.`);
+                }
             } else if (users[name] < guessCount) {
-                alert(`That's correct ${name}! And you did better in your last game by ${guessCount - users[name]} fewer guesses!`);
-            } else if (guessCount < users[name]){
-                alert(`That's correct ${name}! And you beat your previous attempt by ${users[name] - guessCount} fewer guesses!`);
-            } else if (users[name] === guessCount) {
-                alert(`That's correct ${name}! You had the same amount of guesses as your last game!`);
+                alert(`That's correct ${name}! It took you ${guessCount} ${pluralOrNotGuessCount}. You did better in your last game by ${better}  ${pluralOrNotBetter}!`);
+                } else if (guessCount < users[name]) {
+                alert(`That's correct ${name}! It took you ${guessCount} ${pluralOrNotGuessCount}. You beat your previous attempt by ${worse} ${pluralOrNotWorse}!`);
+            } else {
+                alert(`That's correct ${name}! It took you ${guessCount} ${pluralOrNotGuessCount}. You had the same amount of guesses as your last game!`);
             }
             users[name] = guessCount;
-            playAgain = prompt(`Would you like to play again? \"yes\" or \"no\"`);
-            while (playAgain !== "yes" && playAgain !== "no") {
+            playAgain = prompt(playAgainMessage);
+            while (playAgain.toLocaleLowerCase() !== "yes" && playAgain.toLocaleLowerCase() !== "no") {
                 alert(`Sorry ${name}, I didn't catch that.`);
-                playAgain = prompt(`Would you like to play again? \"yes\" or \"no\"`);
+                playAgain = prompt(playAgainMessage);
             }
-            if (playAgain === "yes") {
+            if (playAgain.toLocaleLowerCase() === "yes") {
                 mainGame();
                 break;
             }
         } else if (answer < secretNumber) {
-            answer = Number.parseInt(prompt(`Sorry ${name}, guess higher`));
+            answer = Number.parseInt(prompt(higherMessage));
         } else {
-            answer = Number.parseInt(prompt(`Sorry ${name}, guess lower`));
+            answer = Number.parseInt(prompt(lowerMessage));
         }
-        guesses.push(answer);
-        guessCount++;
+        
     }
 }
 
